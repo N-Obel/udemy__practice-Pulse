b30059d111b61b02b783420c22546438e8aa7a43
Nikita <n.obelets@gmail.com>
2023-06-10 18:55:29 +0300

    finish-footer

--- src/js/script.js
+++ src/js/script.js
@@ -15,9 +15,25 @@ $(document).ready(function () {
 		]
 	});
 
-	$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
 		$(this)
-			.addClass('active').siblings().removeClass('active')
-			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.catalog__container').find('div.catalog__items').removeClass('catalog__items_active').eq($(this).index()).addClass('catalog__items_active');
+	});
+
+	$('.item-catalog__link').each(function (i) {
+		$(this).on('click', function (e) {
+			e.preventDefault();
+			$('.item-catalog__content').eq(i).toggleClass('item-catalog__content_active');
+			$('.item-catalog__list').eq(i).toggleClass('.item-catalog__list_active');
+		})
+	});
+
+	$('.item-catalog__back').each(function (i) {
+		$(this).on('click', function (e) {
+			e.preventDefault();
+			$('.item-catalog__content').eq(i).toggleClass('item-catalog__content_active');
+			$('.item-catalog__list').eq(i).toggleClass('.item-catalog__list_active');
+		})
 	});
 });
